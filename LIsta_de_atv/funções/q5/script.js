97343c78742ab7f1d5a070f624a5f55fca93eb85
Varlennnn <124395176+Varlen123@users.noreply.github.com>
2024-04-13 23:37:59 -0300

    Update script.js

--- LIsta_de_atv/funções/q5/script.js
+++ LIsta_de_atv/funções/q5/script.js
@@ -1,1 +1,18 @@
-//socorro
+let Humbertos = [
+{nome: "varlen", idade: 13, skills: ["JavaScript"]},
+{nome: "Matusalém", idade: 14, skills: ['Python']},
+{nome: "Kayo", idade: 15, skills: ['JavaScript']},
+{nome: "Sales", idade: 16, skills: ["React"] },
+{ nome: "Raphael", idade: 17, skills: ["Java"]}
+];
+function achar(funcao, skill) {
+ for (let Humbertos of funcao) {
+  if (Humbertos.skills.includes(skill)) {
+   console.log(Humbertos);
+        }
+        else{
+   console.log("não encontrado")
+        }
+    }
+}
+achar(Humbertos, "Java");
